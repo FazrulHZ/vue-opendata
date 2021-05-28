84ba67db68e0b58747492170e04fb27c06b39b4f
fazrulhz <ardin260@gmail.com>
2021-05-28 16:21:47 +0800

    Mendinamiskan grupdata

--- src/components/groupdata.vue
+++ src/components/groupdata.vue
@@ -2,14 +2,14 @@
   <div>
     <v-row>
       <v-col cols="12" md="3">
-        <v-card elevation="2">
+        <v-card elevation="2" to="/data">
           <v-row no-gutters class="pa-3">
             <div class="mr-3 my-auto">
               <v-icon size="65" :color="iconColor">mdi-file-document-multiple-outline</v-icon>
             </div>
             <div>
               <div>
-                <h1>900</h1>
+                <h1>{{ jumlahDataset }}</h1>
               </div>
               <div>
                 Dataset
@@ -19,14 +19,14 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="3">
-        <v-card elevation="2">
+        <v-card elevation="2" to="/data">
           <v-row no-gutters class="pa-3">
             <div class="mr-3 my-auto">
               <v-icon size="65" :color="iconColor">mdi-database-outline</v-icon>
             </div>
             <div>
               <div>
-                <h1>7891</h1>
+                <h1>{{ jumlahData }}</h1>
               </div>
               <div>
                 File Data
@@ -36,14 +36,14 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="3">
-        <v-card elevation="2">
+        <v-card elevation="2" to="/organisasi">
           <v-row no-gutters class="pa-3">
             <div class="mr-3 my-auto">
               <v-icon size="65" :color="iconColor">mdi-office-building-outline</v-icon>
             </div>
             <div>
               <div>
-                <h1>35</h1>
+                <h1>{{ jumlahOrganisasi }}</h1>
               </div>
               <div>
                 Organisasi
@@ -53,17 +53,17 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="3">
-        <v-card elevation="2">
+        <v-card elevation="2" to="/grup">
           <v-row no-gutters class="pa-3">
             <div class="mr-3 my-auto">
               <v-icon size="65" :color="iconColor">mdi-format-list-text</v-icon>
             </div>
             <div>
               <div>
-                <h1>27</h1>
+                <h1>{{ jumlahGrup }}</h1>
               </div>
               <div>
-                Topik
+                Grup
               </div>
             </div>
           </v-row>
@@ -76,7 +76,51 @@
 <script>
 export default {
   data: () => ({
-    iconColor: 'primary'
-  })
+    iconColor: 'primary',
+
+    jumlahDataset: 0,
+    jumlahData: 0,
+    jumlahOrganisasi: 0,
+    jumlahGrup: 0
+  }),
+
+  mounted() {
+    this.getDataset()
+    this.getOrganisasi()
+    this.getGrup()
+  },
+
+  methods: {
+    getDataset() {
+      this.http
+        .get(process.env.VUE_APP_API_BASE + 'dataset/')
+        .then(res => {
+          this.jumlahDataset = res.data.count
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    getOrganisasi() {
+      this.http
+        .get(process.env.VUE_APP_API_BASE + 'organisasi/')
+        .then(res => {
+          this.jumlahOrganisasi = res.data.count
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    getGrup() {
+      this.http
+        .get(process.env.VUE_APP_API_BASE + 'grup/')
+        .then(res => {
+          this.jumlahGrup = res.data.count
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
